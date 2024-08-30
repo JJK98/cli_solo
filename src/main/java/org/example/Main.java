5c8fd0a4273feeb55527d5ba23fb7d76d82325d8
JJk <wnsrb1594@naver.com>
2024-08-30 09:31:37 +0900

    level 2,3,4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,20 @@ public class Main {
         System.out.println("=== 게시판 앱 ===");
         Scanner sc = new Scanner(System.in);
         String ord ;
+        int lastid = 0;
         while (true) {
             System.out.print("명령) ");
             ord = sc.nextLine().trim();
             if (ord.equals("종료")){
                 break;
+            }
+            if (ord.equals("등록")){
+                lastid ++;
+                System.out.print("제목 : ");
+                String sub = sc.nextLine().trim();
+                System.out.print("제목 : ");
+                String con = sc.nextLine().trim();
+                System.out.printf("%d번 게시글이 등록되었습니다.\n",lastid);
             }
         }
     }
